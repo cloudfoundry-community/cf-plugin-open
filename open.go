f32c3ce6d2c3be43dd834e665e86663f1a01170b
Dr Nic Williams <drnicwilliams@gmail.com>
2015-02-04 11:45:17 -0600

    cf service-open opens the dashboard URL for a service instance

--- open.go
+++ open.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -9,15 +10,48 @@ import (
 	"github.com/skratchdot/open-golang/open"
 )
 
+type serviceInstanceResponse struct {
+	Entity serviceInstance `json:"entity"`
+}
+
+type serviceInstance struct {
+	DashboardURL string `json:"dashboard_url"`
+}
+
 func main() {
 	plugin.Start(&OpenPlugin{})
 }
 
-//OpenPlugin empty struct for plugin
+// OpenPlugin empty struct for plugin
 type OpenPlugin struct{}
 
-//Run of seeder plugin
+// Run of seeder plugin
 func (plugin OpenPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+	if args[0] == "open" {
+		plugin.runAppOpen(cliConnection, args)
+	} else if args[0] == "service-open" {
+		plugin.runServiceOpen(cliConnection, args)
+	}
+}
+
+// GetMetadata of plugin
+func (OpenPlugin) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "open",
+		Commands: []plugin.Command{
+			{
+				Name:     "open",
+				HelpText: "open app url in browser",
+			},
+			{
+				Name:     "service-open",
+				HelpText: "open service instance dashboard in browser",
+			},
+		},
+	}
+}
+
+func (plugin OpenPlugin) runAppOpen(cliConnection plugin.CliConnection, args []string) {
 	output, err := cliConnection.CliCommandWithoutTerminalOutput("app", args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stdout, "error: app does not exist")
@@ -34,15 +68,31 @@ func (plugin OpenPlugin) Run(cliConnection plugin.CliConnection, args []string) 
 	open.Run(url)
 }
 
-//GetMetadata of plugin
-func (OpenPlugin) GetMetadata() plugin.PluginMetadata {
-	return plugin.PluginMetadata{
-		Name: "open",
-		Commands: []plugin.Command{
-			{
-				Name:     "open",
-				HelpText: "open app url in browser",
-			},
-		},
+func (plugin OpenPlugin) runServiceOpen(cliConnection plugin.CliConnection, args []string) {
+	output, err := cliConnection.CliCommandWithoutTerminalOutput("service", args[1], "--guid")
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "error: service does not exist")
+		os.Exit(1)
+	}
+	serviceInstanceGUID := strings.TrimSpace(output[0])
+
+	output, err = cliConnection.CliCommandWithoutTerminalOutput("curl", fmt.Sprintf("/v2/service_instances/%s", serviceInstanceGUID))
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "error: service does not exist")
+		os.Exit(1)
+	}
+	jsonStr := ""
+	for _, line := range output {
+		jsonStr += line + "\n"
+	}
+
+	response := serviceInstanceResponse{}
+	json.Unmarshal([]byte(jsonStr), &response)
+
+	url := response.Entity.DashboardURL
+	if url == "" {
+		fmt.Println("No dashboard available")
+	} else {
+		open.Run(url)
 	}
 }
